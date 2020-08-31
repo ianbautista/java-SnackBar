124e7fa95cc9d711db5afd2f7014363fa35dc7c7
ianbautista <ianfbautista@gmail.com>
2020-08-31 15:24:06 -0700

    Customer 2 (Bob) buys 2 of snack 4 (Soda).

--- snackBar/src/snackBar/Main.java
+++ snackBar/src/snackBar/Main.java
@@ -35,15 +35,23 @@ public class Main
       	// Processing
       	// Customer 1 (Jane) buys 3 of snack 4 (Soda).
       	jane.buySnacks(soda.getCost(), 3);
-      	System.out.println("Customer 1 cash on hand", jane.getCashOnHand());
+      	System.out.println("Customer 1 cash on hand" + jane.getCashOnHand());
       	soda.snacksBought(3);
-      	System.out.println("Quantity of snack 4", soda.getQuantity());
+      	System.out.println("Quantity of snack 4" + soda.getQuantity());
 
       	// Customer 1 (Jane) buys 1 of snack 3 (Pretzel).
       	jane.buySnacks(pretzel.getCost(), 3);
-      	System.out.println("Customer 1 cash on hand", jane.getCashOnHand());
+      	System.out.println("Customer 1 cash on hand" + jane.getCashOnHand());
       	pretzel.snacksBought(3);
-      	System.out.println("Quantity of snack 3", pretzel.getQuantity());
+      	System.out.println("Quantity of snack 3" + pretzel.getQuantity());
+
+      	// Customer 2 (Bob) buys 2 of snack 4 (Soda).
+      	bob.buySnacks(soda.getCost(), 3);
+      	System.out.println("Customer 1 cash on hand" + bob.getCashOnHand());
+      	soda.snacksBought(3);
+      	System.out.println("Quantity of snack 4" + soda.getQuantity());
+
+
 	}
 
 	public static void main(String[] args)
